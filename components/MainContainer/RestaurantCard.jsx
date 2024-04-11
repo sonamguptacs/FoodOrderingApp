24e8dc7ce49073cff83cec5ab72e0a5c515a0c0f
sonamguptacs <sonamcs3092@gmail.com>
2024-04-11 23:59:58 +0530

    integrated api and layout changes

--- components/MainContainer/RestaurantCard.jsx
+++ components/MainContainer/RestaurantCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-export const RestaurantCard = () => {
+export const RestaurantCard = (props) => {
   return (
     <div className="restaurant-card">
       <img
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/mocil3npmwpcdcfaxql1"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${props.restaurant.info.cloudinaryImageId}`}
         alt="food"
         className="food-image"
       />
       <div className="details">
-        <div className="restaurant-name">Restaurant Name</div>
-        <div className="ratings">4.0</div>
-        <div className="cuisines">Cuisines</div>
+        <div className="restaurant-name">{props.restaurant.info.name}</div>
+        <div className="ratings">{props.restaurant.info.avgRating}</div>
+        <div className="cuisines">
+          {props.restaurant.info.cuisines.join(',')}
+        </div>
       </div>
     </div>
   )
--- components/MainContainer/RestaurantList.jsx
+++ components/MainContainer/RestaurantList.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './list.css'
 import { RestaurantCard } from './RestaurantCard'
 
 export const RestaurantList = () => {
+  const [restaurantList, setRestaurantList] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const response = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
+    )
+    const data = await response.json()
+    setRestaurantList(
+      data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    )
+  }
+
   return (
     <div className="restaurant-list">
-      <RestaurantCard />
+      {restaurantList?.map((restaurant) => (
+        <RestaurantCard restaurant={restaurant} />
+      ))}
     </div>
   )
 }
